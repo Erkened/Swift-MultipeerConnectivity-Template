d91d6092b3cc9082e2ab13c96c3b5dc80f461ad9
John <audioyltd@gmail.com>
2015-04-21 20:07:15 +0100

    Minor change

--- SwiftMultipeerConnectivityTemplate/AppDelegate.swift
+++ SwiftMultipeerConnectivityTemplate/AppDelegate.swift
@@ -2,9 +2,9 @@
 //  AppDelegate.swift
 //  SwiftMultipeerConnectivityTemplate
 //
-//  Created by John on 21/04/2015.
+//  Created by Jonathan Neumann on 21/04/2015.
 //  Copyright (c) 2015 Audio Y. All rights reserved.
-//
+//  www.audioy.co
 
 import UIKit
 
--- SwiftMultipeerConnectivityTemplate/ViewController.swift
+++ SwiftMultipeerConnectivityTemplate/ViewController.swift
@@ -2,9 +2,9 @@
 //  ViewController.swift
 //  SwiftMultipeerConnectivityTemplate
 //
-//  Created by John on 21/04/2015.
+//  Created by Jonathan Neumann on 21/04/2015.
 //  Copyright (c) 2015 Audio Y. All rights reserved.
-//
+//  www.audioy.co
 
 import UIKit
 
