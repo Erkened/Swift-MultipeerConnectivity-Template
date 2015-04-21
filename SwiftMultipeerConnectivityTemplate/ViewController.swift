572d5ead4a1e86b6a979d6c23f33b50bc7fcb545
John <audioyltd@gmail.com>
2015-04-21 19:03:20 +0100

    Initial Commit

---
+++ SwiftMultipeerConnectivityTemplate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftMultipeerConnectivityTemplate
+//
+//  Created by John on 21/04/2015.
+//  Copyright (c) 2015 Audio Y. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
